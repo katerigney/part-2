330b78b4743e1d505d32a62571ca18aadc0abeae
Rigney <krigney@proforma.com>
2018-05-06 20:26:50 -0400

    Notes and demo ready.

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,17 +1,25 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
+//Separate JS file -- this works independently from the main JS functionality
+
+//REGISTER SERVICE WORKER
+    //attaches this service worker to this app/site and defines "scope"
+    
+if ('serviceWorker' in navigator) { //check if browser supports
+    window.addEventListener('load', function () { //listening for page load
         navigator.serviceWorker.register('./serviceWorker.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function (err) {
-            // registration failed :(
+            // registration did not work
             console.log('ServiceWorker registration failed: ', err);
         });
     });
 }
 
-var CACHE_NAME = 'kitties-cache';
-var urlsToCache = [
+//INSTALL SERVICE WORKER
+    //this is where we set up our cache and get the service worker going
+
+var CACHE_NAME = 'kitties-cache'; 
+var urlsToCache = [ //define all the items we want to cache
     './',
     './index.html',
     './index2.html',
@@ -22,16 +30,21 @@ var urlsToCache = [
     './images/kitten2.jpg'
 ];
 
+//uses "cache" interface to achieve a lot of this
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
-            })
-    );
+            }) 
+    ); 
 });
+//you can do a number of CRUD operations 
+//in tandem with user interaction, updates, connectivity changes
 
+
+//MAGIC - CALL AND MATCH THE CACHED FILES SO THE SITE RUNS EVEN WHEN OFFLINE
 self.addEventListener('fetch', function (event) {
     console.log(event.request.url);
     event.respondWith(
